565181a93b0ad9affc7ae0bf53ddfcdbebabfaf1
Brittani S Avery <brittani.s.avery@gmail.com>
2023-08-10 21:27:39 -0500

    Add tailwind css

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,11 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesheet from "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
   return (
@@ -7,6 +14,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
